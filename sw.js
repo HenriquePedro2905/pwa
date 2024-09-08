5374d955c37e8fa4b3549b1a634530d0393c6862
HenriquePedro2905 <136708570+HenriquePedro2905@users.noreply.github.com>
2024-09-08 12:31:57 -0300

    refactor pwa

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+    console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open('pwa-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/manifest.json',
+                '/index.html',
+                '/styles/style.css',
+                '/styles/formLogin.css',
+                '/js/Login.js',
+                '/js/FormToggle.js',
+                '/js/main.js'
+            ]).catch(err => {
+                console.error('Falha ao adicionar arquivos ao cache:', err);
+            });
+        }).catch(err => {
+            console.error('Falha ao abrir o cache:', err);
+        })
+    );
+});
