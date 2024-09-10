d3f06e70f0989b841d182d717c56298b44449537
HenriquePedro2905 <136708570+HenriquePedro2905@users.noreply.github.com>
2024-09-10 19:06:47 -0300

    update service worker

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js')
+          .then(function(registration) {
+            console.log('Service Worker registrado com sucesso:', registration);
+          })
+          .catch(function(error) {
+            console.error('Falha ao registrar o Service Worker:', error);
+          });
+      }
+
     const formLogin = document.getElementById('submit-btn');
 
     formLogin.addEventListener('click', (event) => {
